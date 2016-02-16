a73c05d4ad45ae1cbd5006a56ee9bc257478174b
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2016-02-16 14:43:57 -0700

    working on delete

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -4,7 +4,7 @@ app.controller('HomeController', function($scope) {
 app.controller('DashboardController', function($scope, $http) {
     $http({
         method: 'GET',
-        url: ' https://didgeridone.herokuapp.com/task/56c252022afa221bdcaabcb5',
+        url: ' https://didgeridone.herokuapp.com/task/56c39410b2273e8c7c9d3603',
         headers: {
             'Accept': 'application/json, text/javascript, /; q=0.01',
             'Content-Type': 'application/json; charset=utf-8',
@@ -12,9 +12,20 @@ app.controller('DashboardController', function($scope, $http) {
     }).success(function(data, status) {
         console.log(data.user.tasks[0].name)
         $scope.groups = []
+        $scope.task_id = []
+        $scope.data = []
         for (i = 0; i < data.user.tasks.length; i++) {
             $scope.groups.push(data.user.tasks[i].name)
         }
+        for(i = 0; i<data.user.tasks.length; i++){
+        	$scope.task_id.push(data.user.tasks[i].task_id)
+        }
+        for(i=0; i<data.user.tasks.length; i++){
+        	$scope.data.push(data.user.tasks[i])
+        }
+        console.log($scope.data)
+        console.log($scope.task_id);
+        // console.log(data.user.tasks[4].task_id)
         console.log($scope.groups)
         $scope.index = $scope.groups.length
         $scope.addTask = function() {}
@@ -28,31 +39,44 @@ app.controller('DashboardController', function($scope, $http) {
             isFirstDisabled: false
         };
     }).error(function(data, status) {
-     console.log(status)
+        console.log(status)
     });
-});
-      $scope.postTask = function() {   
-      	$http({
-              url: ' https://didgeridone.herokuapp.com/task/56c252022afa221bdcaabcb5',
-              method: "POST",
-              // data: JSON.stringify({
-            	//names of data parameters
-              //     "name": "",
-              //     "query": ""
-              // }),
-              headers: {
-                  'Content-Type': 'application/json',
-                  // 'apiKey': 'Y2hvd2NoZWNrZXIyMDE1'
-              }
-          }).success(function(data, status, headers, config) {
-           console.log(data)
-          }).error(function(foodData, status, headers, config) {
-              // need to create form validation controller if query is empty
-              console.log(status)
-              })
-              
-                  // $scope.status = status + ' ' + headers;
-      }
-      $scope.deleteTask = function() {
-          
-      }
+    $scope.postTask = function() {
+        $http({
+            url: ' https://didgeridone.herokuapp.com/task/56c39410b2273e8c7c9d3603',
+            method: "POST",
+            // data: JSON.stringify({
+            //names of data parameters
+            //     "name": "",
+            //     "query": ""
+            // }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).success(function(data, status, headers, config) {
+            console.log(data)
+        }).error(function(foodData, status, headers, config) {
+            // need to create form validation controller if query is empty
+            console.log(status)
+        })
+    }
+    $scope.deleteTask = function(index) {
+    	console.log($scope.data)
+    	for(i=0; i<$scope.data.length; i++){
+    	if($scope.data[i].user.task){
+    		console.log($scope.task_id[i])
+    		$http.delete('https://didgeridone.herokuapp.com/task/56c39410b2273e8c7c9d3603/' + task_id[i]).success(function(data, status, headers) {
+                $scope.groups.splice(index, 1)
+        }).error(function(data, status, header, config) {})
+    }
+    	}
+    }
+        
+    $scope.updateTask = function() {
+        $http.put('https://didgeridone.herokuapp.com/task/56c39410b2273e8c7c9d3603/').success(function(response, status, headers, config) {
+            console.log(response)
+        }).error(function(response, status, headers, config) {
+            $scope.error_message = response.error_message;
+        });
+    };
+})
